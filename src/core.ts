ab32642595d1d1a766abcbd11fe98d80dee3b0f3
sipimokus <G1i2t3h4@&>
2015-12-06 19:51:11 +0100

    Formatted code

--- src/core.ts
+++ src/core.ts
@@ -2,63 +2,45 @@
 ///<reference path='./boot/Boot.ts'/>
 
 //import {global} from 'typings/express-go';
-declare function app_path (innerPath?: string, getRelative?: boolean)  : string;
-declare function app_modules (innerPath?: string, getRelative?: boolean)  : string;
+declare function app_path( innerPath? : string, getRelative? : boolean ) : string;
+declare function app_modules( innerPath? : string, getRelative? : boolean ) : string;
 
-var glob  = require("glob");
-var path  = require("path");
-var debug = require('debug')('express-go:core');
+var glob  = require( "glob" );
+var path  = require( "path" );
+var debug = require( 'debug' )( 'express-go:core' );
 
-module.exports = function( /*app : Express.Application,*/ appGlobal : any )
+module.exports = function ( /*app : Express.Application,*/ appGlobal : any )
 {
-    debug("core initalizing");
-    appGlobal.App     = {};
-    appGlobal.Modules = {};
+	debug( "core initalizing" );
+	appGlobal.App     = {};
+	appGlobal.Modules = {};
 
-    debug("Express initalizing");
-    var theApp = require("./express");
-
-
-    // Application boot
-    debug("application booting");
-    debug("boot path: %s", app_path());
-    require("./boot/Boot").Boot.Main( theApp, appGlobal.App );
+	debug( "Express initalizing" );
+	var theApp = require( "./express" );
 
 
-    // Modules boot
-    debug("application modules booting");
-    var files = glob.sync( app_modules("**/module.json") );
-
-    files.forEach(( file ) =>
-    {
-        debug("boot module path: %s", path.dirname(file));
-        var moduleName = path.dirname(file).replace(/\\\//g, "/").split('/').pop().toLowerCase();
-        moduleName = moduleName.substring(0, 1).toUpperCase() + moduleName.substring(1).toLowerCase();
-
-        debug("boot module : %s", moduleName);
-
-        appGlobal.Modules[moduleName] = {};
-
-        require("./boot/Boot").Boot.Main( theApp, appGlobal.Modules[moduleName], path.dirname(file) );
-    });
+	// Application boot
+	debug( "application booting" );
+	debug( "boot path: %s", app_path() );
+	require( "./boot/Boot" ).Boot.Main( theApp, appGlobal.App );
 
 
-//    console.log(require("./boot/Boot.ts").Boot());
-//    require("./boot/Boot.ts").Boot( theApp );
+	// Modules boot
+	debug( "application modules booting" );
+	var files = glob.sync( app_modules( "**/module.json" ) );
 
-    //require("./ExpressGo.ts").Main( theApp );
-    //process.exit();
+	files.forEach( ( file ) =>
+	{
+		debug( "boot module path: %s", path.dirname( file ) );
+		var moduleName = path.dirname( file ).replace( /\\\//g, "/" ).split( '/' ).pop().toLowerCase();
+		moduleName     = moduleName.substring( 0, 1 ).toUpperCase() + moduleName.substring( 1 ).toLowerCase();
 
-    //console.log(require("./boot/boot.ts"));
+		debug( "boot module : %s", moduleName );
 
-    //require("./boot/boot.ts")
-//        .Boot( theApp );
+		appGlobal.Modules[ moduleName ] = {};
 
-    //require("./boot/boot.ts")
-//        .Core.Boot.Boot( theApp );
+		require( "./boot/Boot" ).Boot.Main( theApp, appGlobal.Modules[ moduleName ], path.dirname( file ) );
+	} );
 
-    //require("./bootstrap.ts")
-        //.Core.Bootstrap( theApp, appGlobal );
-
-    return theApp;
+	return theApp;
 };
