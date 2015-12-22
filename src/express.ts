3c908ff6f1c42b82550f24a69e256af181626c2a
sipimokus <G1i2t3h4@&>
2015-12-23 00:53:32 +0100

    Translator loader moved

--- src/express.ts
+++ src/express.ts
@@ -19,9 +19,7 @@ var helmet        = require( 'helmet' );
 var logger        = require( 'morgan' );
 var nodalytics    = require( 'nodalytics' );
 var session       = require( 'express-session' );
-var i18nxt        = require( 'i18next' );
-var i18nxtFSB     = require( 'i18next-node-fs-backend' );
-var i18nxtSprintf = require( 'i18next-sprintf-postprocessor' );
+
 
 var redis       = require( 'redis' );
 var redisClient = redis.createClient();
@@ -29,12 +27,6 @@ var RedisStore  = require( "connect-redis" )( session );
 
 var debug = require( 'debug' )( 'express-go:Express' );
 
-// Settings
-var languages = [
-	'dev',
-	'hu',
-	'en'
-];
 
 
 debug( "Express object created" );
@@ -71,12 +63,8 @@ class ExpressGo
 	{
 		debug( "ExpressGo init app" );
 
-
 		debug( "ExpressGo init initParsers" );
 		this.initParsers();
-
-		debug( "ExpressGo init initTranslator" );
-		this.initTranslator();
 
 		debug( "ExpressGo init initStatics" );
 		this.initStatics();
@@ -117,121 +105,6 @@ class ExpressGo
 		} );
 
 	}
-
-	/**
-	 * Translator i18next
-	 */
-	private initTranslator()
-	{
-		// Read language files for namespaces
-		var langNs = [ "translation" ];
-		var files  = glob.sync( global.lang_path( "**/*.json" ) );
-
-		files.forEach( ( file ) =>
-		{
-			var tmpFile = path.basename( file ).split( '.' );
-			var tmpNs   = tmpFile[ 0 ] != 'new' ? tmpFile[ 0 ] : tmpFile[ 1 ];
-
-			if ( langNs.indexOf( tmpNs ) === -1 )
-				langNs.push( tmpNs );
-
-		} );
-
-		// i18next
-		i18nxt
-			.use( i18nxtFSB )
-			.use( i18nxtSprintf )
-			.init( {
-				debug : process.env.APP_DEBUG,
-
-				lng				: 'en',
-				fallbackLng		: [ 'dev' ],
-
-				ns				: langNs,
-				defaultNS		: "translation",
-				fallbackNS		: "translation",
-
-				whitelist		: languages,
-				preload			: languages,
-
-				keySeparator	: ".",
-				nsSeparator		: ":",
-
-				saveMissing		: true,
-				saveMissingTo	: "fallback",	// all, fallback, current
-
-				// V2.x
-				backend			:
-				{
-					loadPath	: global.lang_path( "/{{lng}}/{{ns}}.json" ),
-					addPath		: global.lang_path( "/{{lng}}/new.{{ns}}.json" ),
-					jsonIndent	: 2
-				},
-				ignoreRoutes 	:
-				[
-					'images/', 'public/', 'css/', 'js/', 'assets/', 'img/'
-				],
-
-				// right now there is only the integrated console logger available.
-				/*functions		: {
-				 log : require( 'debug' )( 'express-go:i18n' )
-				 },*/
-			} );
-
-
-		app.i18n = i18nxt;
-
-		// Use middleware to set current language
-		// ?lang=xx_yy
-		app.use( function ( req : any, res : any, next : any )
-		{
-			// Session lang init
-			if ( !req.session.lang )
-			{
-				req.session.lang = app.i18n.language;
-			}
-
-			// URL ignoring
-			var ignore = i18nxt.options.ignoreRoutes;
-			for ( var i = 0, len = ignore.length; i < len; i++ )
-			{
-				if ( req.path.indexOf( ignore[ i ] ) > -1 )
-				{
-					return next();
-				}
-			}
-
-			// Query land settings
-			if ( req.query.lang != undefined && languages.indexOf( req.query.lang ) >= 0 )
-			{
-				req.session.lang = req.query.lang;
-
-			}
-
-			// Vars
-			//req.locale = req.lng /*= req.language*/ = req.session.lang;
-			res.locals.i18n = app.i18n;
-			res.locals._t   = app.i18n.t;
-			res.locals.__   = app.i18n.t;
-
-			// Set lang if need
-			if ( req.session.lang != app.i18n.language )
-			{
-				app.i18n.changeLanguage( req.session.lang, function ()
-				{
-					next();
-				} );
-
-			}
-			else
-			{
-				next();
-			}
-
-		} );
-
-	}
-
 
 	/**
 	 * Force SSL redirect from HTTP
